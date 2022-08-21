f2a47f36228e58b6e002b05e6befd9531f0444ad
Eva S <evamarie.songco@gmail.com>
2022-08-20 23:07:01 -0400

    Write function allNumbersPositive check function

--- script.js
+++ script.js
@@ -73,3 +73,10 @@ getLastName(["Ash","Stu"]);
 // Write a function that takes an array of numbers and returns true if all of the numbers are positive. It should return false if there are one or more negative numbers in the array.
 // allNumbersPositive([2,4,5]); // returns true
 // allNumbersPositive([-5,4,6]); // returns false
+
+const allNumbersPositive = (numberArray) => {
+      console.log (numberArray.every(numberArray => numberArray >= 0));
+}
+
+allNumbersPositive ([2,4,5]);
+allNumbersPositive ([-5,4,6]);
